f346de4d41f66fc0d4af5534ce13a70e988308fd
Igor <igor.a.vereshchagin@gmail.com>
2022-03-19 18:56:47 +0300

    28.4.1 done

--- 1/main.cpp
+++ 1/main.cpp
@@ -3,11 +3,15 @@
 #include <vector>
 #include <map>
 #include <string>
+#include <mutex>
 
 void inputCompetitors(int n, std::vector<std::pair<std::string, double>> &outCompetitors);
-void someFunc(std::pair<std::string, double> &inPair);
-void race(const std::pair<std::string, double> &competitor, );
+void swimming(const std::pair<std::string, double> &competitor);
 
+std::mutex cout_access;
+std::mutex results_access;
+
+std::map<double, std::vector<std::string>> results;
 
 int main()
 {
@@ -15,12 +19,44 @@ int main()
 
   inputCompetitors(6, competitors);
 
-  for (int i = 0; i < 6; i++)
-    someFunc(competitors[i]);
+  std::thread *race[6];
 
+  for (int i = 0; i < 6; i++)
+    race[i] = new std::thread(swimming, competitors[i]);
+
+  for (int i = 0; i < 6; i++)
+    race[i]->join();
+  std::cout << "======== Results =========" << std::endl;
+  for (std::map<double, std::vector<std::string>>::iterator it = results.begin(); it != results.end(); it ++)
+  {
+    for (int i = 0; i < it->second.size(); i++)
+      std::cout << it->second[i] << ": " << it->first / 1000000 << std::endl;
+  }
   return 0;
 }
 
+void swimming(const std::pair<std::string, double> &competitor)
+{
+  double distance = 100;
+  double ksi = 0.0001;
+  double result = 0;
+  while(distance > 0 && fabs(distance) > ksi)
+  {
+    double speed = competitor.second;
+    int sleepTimeUs = (distance > speed) ? 1000000 : floor((distance * 1000000) / speed + 0.5);
+    std::this_thread::sleep_for(std::chrono::microseconds (sleepTimeUs));
+    distance -= sleepTimeUs * speed / 1000000;
+    result += sleepTimeUs;
+    cout_access.lock();
+    std::cout << "Competitor " << competitor.first << " swam " << 100.0 - distance << " meters" << std::endl;
+    cout_access.unlock();
+  }
+  results_access.lock();
+  if (results.find(result) == results.end())
+    results[result] = std::vector<std::string>();
+  results[result].push_back(competitor.first);
+  results_access.unlock();
+}
 
 void inputCompetitors(int n, std::vector<std::pair<std::string, double>> &outCompetitors)
 {
@@ -35,9 +71,4 @@ void inputCompetitors(int n, std::vector<std::pair<std::string, double>> &outCom
     std::cin >> speed;
     outCompetitors.push_back(std::make_pair(name, speed));
   }
-}
-
-void someFunc(std::pair<std::string, double> &inPair)
-{
-  std::cout << inPair.first << " " << inPair.second << std::endl;
 }
